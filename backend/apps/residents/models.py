df64a2a10c38c0ae9debb8a49d092cbea72c21f3
Miguel Regidor García <migreggar@alum.us.es>
2026-03-02 00:23:44 +0100

    fix(residents.models): added attr verbose names

--- backend/apps/residents/models.py
+++ backend/apps/residents/models.py
@@ -5,18 +5,25 @@ from django.db import models
 
 class Resident(models.Model):
     class ResidentState(models.TextChoices):
-        ACTIVE = "active", "Active"
-        INACTIVE = "inactive", "Inactive"
-        PENDING = "pending", "Pending"
-        
-    fullname = models.CharField(max_length=100)
-    age = models.IntegerField()
-    email = models.EmailField(unique=True)
-    state = models.CharField(max_length=20, choices=ResidentState.choices, default=ResidentState.PENDING)
-    room = models.CharField(max_length=10)
-    building = models.CharField(max_length=100, )
-    # No se si se va a hacer con la del momento o qeu se pueda introducir
-    check_in_date = models.DateField(default=datetime.datetime.now(tz=timezone.utc).date())
+        ACTIVE = "active", "Activo"
+        INACTIVE = "inactive", "Inactivo"
+        PENDING = "pending", "Pendiente"
+
+    fullname = models.CharField(max_length=100, verbose_name="Nombre Completo")
+    age = models.IntegerField(verbose_name="Edad")
+    email = models.EmailField(unique=True, verbose_name="Correo Electrónico")
+    state = models.CharField(
+        max_length=20,
+        choices=ResidentState.choices,
+        default=ResidentState.PENDING,
+        verbose_name="Estado"
+    )
+    room = models.CharField(max_length=10, verbose_name="Habitación")
+    building = models.CharField(max_length=100, verbose_name="Edificio")
+    check_in_date = models.DateField(
+        default=datetime.datetime.now(tz=timezone.utc).date(),
+        verbose_name="Fecha de Registro"
+    )
 
     def __str__(self):
         return self.fullname
