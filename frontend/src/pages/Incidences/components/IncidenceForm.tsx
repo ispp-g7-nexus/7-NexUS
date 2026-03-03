c82cc2b7de16a9e306261e13ff42f6a20f45be3c
Paula Maria Suarez Linares <a.pausualin@gmail.com>
2026-03-03 16:19:24 +0100

    fix: imports fixed

--- frontend/src/pages/Incidences/components/IncidenceForm.tsx
+++ frontend/src/pages/Incidences/components/IncidenceForm.tsx
@@ -6,15 +6,10 @@ import { Button } from "../../../components/ui/button"
 import { Input } from "../../../components/ui/input"
 import { Label } from "../../../components/ui/label"
 import { Checkbox } from "../../../components/ui/checkbox"
-import { 
-  Select, 
-  SelectContent, 
-  SelectItem, 
-  SelectTrigger, 
-  SelectValue 
-} from "../../../components/ui/select"
+import { Select} from "../../../components/ui/select"
 import { DialogDescription } from "../../../components/ui/dialog"
 import { fetchWithAuth, API_URL_INCIDENCES } from "../../../utils/api"
+import { SelectTrigger, SelectValue, SelectContent, SelectItem } from "@radix-ui/react-select"
 //import {API_URL_INCIDENCES } from "../../../utils/api"
 
 interface IncidenceFormProps {
